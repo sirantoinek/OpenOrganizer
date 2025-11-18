0755fe3aea9a9d418d70d76007b08961a8e9497d
Michael Jagiello <michaeljr623@hotmail.com>
2025-11-18 18:42:30 -0500

    feat: add auto notification generation on the start of each day to ensure none are ever missed

--- client/src-electron/services/notifs.ts
+++ client/src-electron/services/notifs.ts
@@ -1,7 +1,7 @@
 /*
  * Authors: Michael Jagiello
  * Created: 2025-11-08
- * Updated: 2025-11-16
+ * Updated: 2025-11-18
  *
  * This file defines all main functionality for creating, maintaining, and executing notifcations based on reminders and generated reminders.
  *
@@ -11,10 +11,11 @@
  */
 
 import * as cron from 'node-cron'
-import { Notification } from "electron";
+import { Notification } from 'electron';
 import { getDayOfYear, type Timestamp } from '@quasar/quasar-ui-qcalendar';
 import { convertTimeAndDateToTimestamp } from "src/frontend-utils/time";
-import type { GeneratedReminder, Override, Reminder } from '../types/shared-types';
+import type { GeneratedReminder, Override, Reminder, RangeWindow } from "../types/shared-types";
+import { readRemindersInRange, readGeneratedRemindersInRange } from "../db/sqlite-db";
 
 interface Notif {
   itemID: bigint,
@@ -31,6 +32,9 @@ const notifications = new Map<bigint, Notif>();
 export function InitNotifications() {
   // run at the start of every minute
   cron.schedule("* * * * *", () => { CheckNotifications() });
+  // ensure that all notifications for current day are loaded
+  void PullToday();
+  cron.schedule("0 0 * * *", () => { void PullToday() });
 }
 
 function CheckNotifications() {
@@ -38,6 +42,48 @@ function CheckNotifications() {
   for (const [key, notif] of notifications) {
     if (notif.time == now) {
       Notify(notif);
+    }
+  }
+}
+
+async function PullToday() {
+  await sleep(10);
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+  const minuteOfToday = (hour * 60 + minute);
+  const dateString = year.toString() + "-" + month.toString() + "-" + day.toString();
+  const startToday = convertTimeAndDateToTimestamp(dateString, "00:00");
+  const startMinOfYear = getDayOfYear(startToday) * 1440 + startToday.hour * 60 + startToday.minute;
+  const rangeWindow: RangeWindow = {
+    startYear: year,
+    startMinOfYear: startMinOfYear,
+    endYear: year,
+    endMinOfYear: startMinOfYear + 1439
+  };
+  const reminders = readRemindersInRange(rangeWindow);
+  const generated = readGeneratedRemindersInRange(rangeWindow);
+  if (reminders != undefined) {
+    for (const reminder of reminders) {
+      if (reminder.notifYear == year && 
+          reminder.notifDay == getDayOfYear(startToday) && 
+          reminder.notifMin == minuteOfToday) {
+        const notif = ReminderToNotif(reminder);
+        Notify(notif);
+      }
+    }
+  }
+  if (generated != undefined) {
+    for (const reminder of generated) {
+      if (reminder.notifYear == year && 
+          reminder.notifDay == getDayOfYear(startToday) && 
+          reminder.notifMin == minuteOfToday) {
+        const notif = GeneratedToNotif(reminder);
+        Notify(notif);
+      }
     }
   }
 }
@@ -55,12 +101,7 @@ export function SetNotifReminder(reminder: Reminder) {
     DeleteNotif(reminder.itemID);
     return false;
   }
-  const notif: Notif = {
-    itemID: reminder.itemID,
-    time: TimeToBigint(reminder.notifYear, reminder.notifDay, reminder.notifMin),
-    title: reminder.title,
-    body: ""
-  };
+  const notif = ReminderToNotif(reminder);
   notifications.set(CombineBigints64(notif.itemID, notif.itemID), notif);
   return true;
 }
@@ -72,13 +113,7 @@ export function SetNotifGenerated(reminder: GeneratedReminder) {
     DeleteNotifGenerated(reminder);
     return false;
   }
-  const notif: Notif = {
-    itemID: reminder.itemID,
-    origEventStartTime: TimeToBigint(reminder.origEventStartYear, reminder.origEventStartDay, reminder.origEventStartMin),
-    time: TimeToBigint(reminder.notifYear, reminder.notifDay, reminder.notifMin),
-    title: reminder.title,
-    body: ""
-  };
+  const notif = GeneratedToNotif(reminder);
   notifications.set(CombineBigints64(notif.itemID, notif.origEventStartTime!), notif);
   return true;
 }
@@ -97,8 +132,32 @@ export function DeleteNotifGenerated(reminder: GeneratedReminder) {
 
 // helpers
 
-// gets current time, formatted as a bigint with 4 bytes for year, 2 bytes for day of year, and 2 bytes for minute
-function GetNowTime() {
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function ReminderToNotif(reminder: Reminder) {
+  const notif: Notif = {
+    itemID: reminder.itemID,
+    time: TimeToBigint(reminder.notifYear, reminder.notifDay, reminder.notifMin),
+    title: reminder.title,
+    body: ""
+  };
+  return notif;
+}
+
+function GeneratedToNotif(reminder: GeneratedReminder) {
+  const notif: Notif = {
+    itemID: reminder.itemID,
+    origEventStartTime: TimeToBigint(reminder.origEventStartYear, reminder.origEventStartDay, reminder.origEventStartMin),
+    time: TimeToBigint(reminder.notifYear, reminder.notifDay, reminder.notifMin),
+    title: reminder.title,
+    body: ""
+  };
+  return notif;
+}
+
+function GetNowTimestamp() {
   const now = new Date();
   const year = now.getFullYear();
   const month = now.getMonth() + 1;
@@ -107,8 +166,12 @@ function GetNowTime() {
   const minute = now.getMinutes();
   const dateString = year.toString() + "-" + month.toString() + "-" + day.toString();
   const timeString = hour.toString() + ":" + minute.toString() + ":00";
-  const time = convertTimeAndDateToTimestamp(dateString, timeString);
-  return TimestampToBigint(time);
+  return convertTimeAndDateToTimestamp(dateString, timeString);
+}
+
+// gets current time, formatted as a bigint with 4 bytes for year, 2 bytes for day of year, and 2 bytes for minute
+function GetNowTime() {
+  return TimestampToBigint(GetNowTimestamp());
 }
 
 // converts Timestamp into a bigint
